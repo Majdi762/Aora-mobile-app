663320e6e6f0e16c5aad9a227ffa7e46b94024b7
majdi zaineb <zaineb.genieinfo@gmail.com>
2024-11-07 00:11:40 +0100

    feat: add bottom tabs

--- app/index.jsx
+++ app/index.jsx
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
     <Text className="text-3xl font-pblack">Aora!</Text>   
-    <Link href='./profile' className="text-blue-600">go to profile</Link>
+    <Link href='./home' className="text-blue-600">Go to Home</Link>
       <StatusBar style="auto" />
     </View>
   );
